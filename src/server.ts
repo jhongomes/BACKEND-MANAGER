971e892a391226f1e97725b43f4810d786691313
jhongomes <jhonatangomes859@gmail.com>
2021-06-03 13:47:09 -0400

    Configurando server e Adicionando interfacer Erros

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import 'express-async-errors';
+import 'reflect-metadata';
+
+
+import express, { Request, Response, NextFunction } from 'express';
+
+
+import AppError from './errors/AppError';
+
+
+const app = express();
+ 
+
+app.use(express.json())
+
+app.use((err: Error, request: Request, response:Response, _:NextFunction) =>{
+
+        if(err instanceof AppError){
+            return response
+            .status(err.statusCode)
+            .json({ status: 'error', message: err.message});
+        }
+        
+        return response
+        .status(500)
+        .json({ status: 'error', message: 'Internal server error' });
+
+
+});
+app.listen(3333);
